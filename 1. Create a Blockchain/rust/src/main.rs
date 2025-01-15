aed0becb809dba4d78437f122edba4f4120ab704
ohohoh3275 <ojy940716@gmail.com>
2025-01-15 22:19:15 +0900

    copy(rust): convert python to rust

--- 1. Create a Blockchain/rust/src/main.rs
+++ 1. Create a Blockchain/rust/src/main.rs
@@ -1,13 +1,26 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+// blockchain object
+
+
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
+#[get("/get_chain")]
+async fn get_chain(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
+}
+
+#[get("/mine_block")]
+async fn mine_block(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[get("/is_valid")]
+async fn is_valid(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body("is_valid")
 }
 
 async fn manual_hello() -> impl Responder {
@@ -19,7 +32,9 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
-            .service(echo)
+            .service(get_chain)
+            .service(mine_block)
+            .service(is_valid)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
