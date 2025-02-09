526e025a6e4cb548f71c8296355c10e4199a0c09
ohohoh3275 <ojy940716@gmail.com>
2025-02-09 19:36:44 +0900

    feat: proof of work

--- 1. Create a Blockchain/rust/src/main.rs
+++ 1. Create a Blockchain/rust/src/main.rs
@@ -1,6 +1,7 @@
 use std::time::SystemTime;
 
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use sha2::{Sha256, Digest};
 
 #[derive(Clone)]
 struct Block {
@@ -17,6 +18,7 @@ struct Blockchain {
 trait BlockchainTrait {
     fn create_block(&mut self, proof: String, previous_hash: String) -> Option<&Block>;
     fn get_previous_block(&mut self) -> Option<&Block>;
+    fn proof_of_work(&mut self, previous_proof: String) -> i32;
 }
 
 impl BlockchainTrait for Blockchain {
@@ -37,6 +39,28 @@ impl BlockchainTrait for Blockchain {
     fn get_previous_block(&mut self) -> Option<&Block> {
         return self.chain.last(); // last() 는 Option<T> 
     }
+
+    fn proof_of_work(&mut self, previous_proof: String) -> i32 {
+        let mut new_proof=1;
+        let mut check_proof:bool=false;
+        while check_proof == false {
+            let mut hasher = Sha256::new();
+            let s: String = previous_proof.to_string();
+            let previous_proof_number: i32 = s.parse().expect("Not a valid number");
+            hasher.update(new_proof*2 - previous_proof_number*2); // !
+            let hash_operation = hasher.finalize();
+
+            if hash_operation[-4] == 0000 {
+                check_proof = true
+            } else {
+                new_proof += 1
+            }
+        }
+
+        return new_proof
+    }
+
+
 }
 
 #[get("/")]
